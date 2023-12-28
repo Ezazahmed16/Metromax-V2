194d770c871c7043702252908a036ba691bbefa1
Ezaz Ahmed <ezazrahul794@gmail.com>
2023-12-28 16:12:36 +0600

    'test'

--- src/components/SouthMetromaxComponent.jsx
+++ src/components/SouthMetromaxComponent.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 const SouthMetromaxComponent = () => {
 
@@ -29,21 +31,68 @@ const SouthMetromaxComponent = () => {
 
         <div className="grid  grid-cols-1 md:grid-cols-2 gap-5 h-screen">
           <div className="">
-            <h1 style={{ borderLeft: '4px solid #4E0D38' }} className='text-xl md:text-3xl border-l-8 pl-5 text-justify'>
+            <h1 style={{ borderLeft: '4px solid #4E0D38' }} className='text-xl md:text-3xl border-l-8 pl-5 text-justify mb-10'>
               INTERIOR</h1>
 
             <div className="">
-              
+              <AwesomeSlider>
+                <div>
+                  <img src='https://i.ibb.co/HNYYypM/Whats-App-Image-2023-12-17-at-16-48-42-8deb12d8.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/F4t9KWV/Whats-App-Image-2023-12-17-at-16-48-56-23f57237.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/F4t9KWV/Whats-App-Image-2023-12-17-at-16-48-56-23f57237.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/J2HK0x6/Whats-App-Image-2023-12-17-at-16-48-43-85bce390.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+              </AwesomeSlider>
             </div>
           </div>
 
           <div className="">
-            <h1 style={{ borderLeft: '4px solid #4E0D38' }} className='text-xl md:text-3xl border-l-8 pl-5 text-justify'>
+            <h1 style={{ borderLeft: '4px solid #4E0D38' }} className='text-xl md:text-3xl border-l-8 pl-5 text-justify mb-10'>
               EXTERIOR</h1>
 
-              <div className="">
-                
-              </div>
+            <div className="">
+              <AwesomeSlider>
+                <div className=''>
+                  <img src='https://i.ibb.co/GvhpcKB/1.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/cN3M2m5/2.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/n8WbTLn/4.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/4SBCfWN/3.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/1qMCDDF/5.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+                <div>
+                  <img src='https://i.ibb.co/KsbzRQg/6.jpg' alt="" className="w-screen md:w-full h-96 rounded-xl" />
+                </div>
+              </AwesomeSlider>
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <h1 style={{ borderLeft: '4px solid #4E0D38' }} className='text-xl md:text-3xl border-l-8 pl-5 text-justify'>
+            AMENITIES</h1>
+
+          <div className="">
+            <div className="">
+
+              {/* <figure className="flex-all rounded mb-15 m-side-auto">
+                <img alt="" src="https://imtiaz.ae/uploads/facility/3/Fully-Furnished.svg" />
+              </figure> */}
+
+            </div>
           </div>
         </div>
 
